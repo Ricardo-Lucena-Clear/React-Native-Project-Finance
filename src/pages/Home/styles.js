8eb84f99cfe65886152b78d7c12522bf91894fa3
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-06-21 11:25:03 -0300

    Customizing our Drawer

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -48,4 +48,12 @@ export const List = styled.FlatList.attrs({
 
 `;
 
-
+export const ListaVazia = styled.View`
+    padding-top: 15px;
+    background-color: #FFF;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    margin-left: 8px;
+    margin-right: 8px;
+    height: 100%
+`;
